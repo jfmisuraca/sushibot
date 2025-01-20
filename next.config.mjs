66dccc53ecfd633699ef06c4249a490ee98708b1
Juan Francisco <jfmisuraca@hotmail.com>
2025-01-20 07:51:55 -0300

    esto va a dar error, cierto?

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
   webpack: (config) => {
     config.externals = [...config.externals, 'canvas', 'jsdom']
     return config
   }
 }
 
-module.exports = nextConfig 
+export default nextConfig 
